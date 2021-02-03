e684b48104c0baa61bf5e9c26471f17ef3f99ea2
Dawid <dawidw23@poczta.onet.pl>
2021-02-03 19:59:18 +0100

    Changing on layout_main when mainWindowButton clicked

--- app/src/main/java/it/dawidwojdyla/myfirstdemoapp/MainActivity.java
+++ app/src/main/java/it/dawidwojdyla/myfirstdemoapp/MainActivity.java
@@ -3,6 +3,7 @@ package it.dawidwojdyla.myfirstdemoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_welcome);
+
+        Button mainWindowButton = findViewById(R.id.btn_next);
+        mainWindowButton.setOnClickListener(l -> setContentView(R.layout.layout_main));
     }
 }
