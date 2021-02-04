f68b901c6dab7665201553bb5deb2f4db4e37909
Dawid <dawidw23@poczta.onet.pl>
2021-02-04 10:55:33 +0100

    Skeleton of request in thread and show message in messageTextView (toast from thread don't work...)

--- app/src/main/java/it/dawidwojdyla/myfirstdemoapp/MainActivity.java
+++ app/src/main/java/it/dawidwojdyla/myfirstdemoapp/MainActivity.java
@@ -4,15 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView messageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_welcome);
 
-        Button mainWindowButton = findViewById(R.id.btn_next);
-        mainWindowButton.setOnClickListener(l -> setContentView(R.layout.layout_main));
+        Button showMainWindowButton = findViewById(R.id.btn_next);
+
+        //Handler handler = new Handler(Looper.getMainLooper());
+
+        showMainWindowButton.setOnClickListener(l -> {
+            manageMainWindow();
+            makeRequestInThread();
+
+        });
+    }
+
+    private void manageMainWindow() {
+
+        setContentView(R.layout.layout_main);
+
+        Button sendButton = findViewById(R.id.btn_send);
+        messageTextView = findViewById(R.id.tv_message);
+
+        sendButton.setOnClickListener(l -> {
+            makeRequestInThread();
+        });
+    }
+
+    private void makeRequestInThread() {
+        Thread thread = new Thread(() -> {
+            try {
+                //Do some request and and show message
+
+                showMessageToast("message");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+
+    private void showMessageToast(String message) {
+        //Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        messageTextView.setText(message);
+
     }
 }
